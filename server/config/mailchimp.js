4604a86a7546934edb9e21b6acac92b2ca1c77f1
Martin-Stefko <mato.stefko@gmail.com>
2021-05-10 13:24:07 +0200

    Lookup for api key

--- server/config/mailchimp.js
+++ server/config/mailchimp.js
@@ -3,20 +3,21 @@ const Mailchimp = require('mailchimp-api-v3');
 const keys = require('./keys');
 
 const { key, listKey } = keys.mailchimp;
+console.log(key);
 const mailchimp = new Mailchimp(key);
 
-exports.subscribeToNewsletter = email => {
-  return new Promise((resolve, reject) => {
-    mailchimp
-      .post(`lists/${listKey}/members`, {
-        email_address: email,
-        status: 'subscribed'
-      })
-      .then(result => {
-        resolve(result);
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
+exports.subscribeToNewsletter = (email) => {
+	return new Promise((resolve, reject) => {
+		mailchimp
+			.post(`lists/${listKey}/members`, {
+				email_address: email,
+				status: 'subscribed'
+			})
+			.then((result) => {
+				resolve(result);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
 };
