9c7a558f0fcbe708ce1d66f4cb323624ae97ae8b
Martin-Stefko <mato.stefko@gmail.com>
2021-05-13 15:50:57 +0200

    Updated manifest.json

--- client/src/containers/UnderConstruction/index.js
+++ client/src/containers/UnderConstruction/index.js
@@ -22,7 +22,7 @@ const UnderConstruction = () => {
 		<div className="under-cons-box">
 			<img src="/images/sf-logo-stacked-dark.svg" alt="sf-logo-stacked.svg" />
 			<div className="block-title">
-				<h1 className="text-center">We are building Online academy 3.0! Stay in touch!</h1>
+				<h1 className="text-center">We are building Online academy 3.0! Stay in touch! :)</h1>
 				<Newsletter />
 			</div>
 		</div>
