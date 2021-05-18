093e0ebe604fd46593a9131f7f791c1510dab537
Martin-Stefko <mato.stefko@gmail.com>
2021-05-18 13:03:06 +0200

    Created deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git push live main
+ssh root@167.99.138.73 "rm -rf /var/www/StrongFacultyCS/server/.env"
+scp /home/martin/Desktop/StrongFacultyCS/server/.env root@167.99.138.73:/var/www/StrongFacultyCS/server/
+ssh root@167.99.138.73 "mkdir -p /var/www/StrongFacultyCS/dhparam/"
+scp /home/martin/Desktop/StrongFacultyCS/dhparam/dhparam-2048.pem root@167.99.138.73:/var/www/StrongFacultyCS/dhparam
