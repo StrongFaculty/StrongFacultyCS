ff2cf9278bc2b4120aadbb5ef134e84f18f7c6dd
Martin-Stefko <mato.stefko@gmail.com>
2021-05-19 11:55:48 +0200

    Swiched context

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,4 +4,4 @@ export const BASE_CLIENT_URL = 'http://localhost:3000';
 export const BASE_SERVER_URL = 'http://localhost:5000';
 export const PRODUCTION_CLIENT_URL = 'https://strongfaculty.com';
 export const PRODUCTION_SERVER_URL = 'https://strongfaculty.com';
-export const NODE_ENV = 'development';
+export const NODE_ENV = 'production';
